95196196411836683e92e2a9a29ec85098877adc
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-17 22:43:26 +0530

    Create DOJ.java

---
+++ CIWithDependantObj/src/main/java/com/demo/CIWithDependantObj/DOJ.java
@@ -0,0 +1,23 @@
+package com.demo.CIWithDependantObj;
+
+public class DOJ {
+
+	
+	  private int date,year;
+	  private String month;
+	public DOJ(int date, int year, String month) {
+		super();
+		this.date = date;
+		this.year = year;
+		this.month = month;
+	}
+	@Override
+	public String toString() {
+		return "DOJ [date=" + date + ", year=" + year + ", month=" + month + "]";
+	}
+	  
+	  
+	  
+	  
+	
+}
